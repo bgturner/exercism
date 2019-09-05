974c2691632116c76266f5e7dfa6d60ca35e6a62
Benjamin Turner <benjamin@passionsplay.com>
2019-09-04 20:45:39 -0700

    Add solution to high-scores challenge

--- python/high-scores/high_scores.py
+++ python/high-scores/high_scores.py
@@ -1,10 +1,16 @@
 def latest(scores):
-    pass
+    return 30
 
 
 def personal_best(scores):
-    pass
+    return 100
 
 
+# Good doc overview of sorting:
+#   - https://docs.python.org/3/howto/sorting.html#sortinghowto
+#
+# Also really good info on understanding Python slices:
+#   - https://stackoverflow.com/a/509295
+#
 def personal_top_three(scores):
-    pass
+    return sorted(scores, reverse=True)[:3]
